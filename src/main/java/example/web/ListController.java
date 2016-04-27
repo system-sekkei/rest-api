447e427e83e111c7f618f794b195b554c868d7ea
masuda <masuda220@gmail.com>
2016-04-27 11:10:31 +0900

    土台にした USERS 関係を一式削除

--- src/main/java/example/web/ListController.java
+++ src/main/java/example/web/ListController.java
@@ -2,7 +2,6 @@ package example.web;
 
 import example.model.user.UserSummaries;
 import example.model.user.UserSummary;
-import example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
