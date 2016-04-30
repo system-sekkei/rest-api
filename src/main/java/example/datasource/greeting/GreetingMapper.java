2b86353114297739565ce9521c0514ace63fb450
masuda220 <masuda220@gmail.com>
2016-04-30 14:01:39 +0900

    MyBatis-Sprint-Boot-Starter 組み込み（ JSR-310 ハンドラ）

--- src/main/java/example/datasource/greeting/GreetingMapper.java
+++ src/main/java/example/datasource/greeting/GreetingMapper.java
@@ -1,11 +1,13 @@
 package example.datasource.greeting;
 
 import example.model.greeting.Greeting;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
+@Mapper
 public interface GreetingMapper {
     void register(@Param("greeting") Greeting greeting);
     List<Greeting> list();
