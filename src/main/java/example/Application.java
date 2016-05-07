baad5076492914b7ff304250d597f47278f38dd1
masuda220 <masuda220@gmail.com>
2016-05-07 22:14:08 +0900

    メッセージファイルを追加

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/example/presentation/controller/greeting/GreetingController.java
+++ src/main/java/example/presentation/controller/greeting/GreetingController.java
@@ -1,4 +1,4 @@
-package example.presentation.controller;
+package example.presentation.controller.greeting;
 
 import example.domain.model.greeting.Greeting;
 import example.domain.model.greeting.GreetingHistory;
@@ -7,6 +7,8 @@ import example.presentation.view.greeting.GreetingResponse;
 import example.presentation.view.greeting.GreetingRequest;
 import example.presentation.view.greeting.HistoryResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -21,11 +23,16 @@ public class GreetingController {
     GreetingService greetingService;
 
     @ApiOperation(value = "挨拶の取得", nickname = "挨拶 GET", notes = "名前を渡すと挨拶を返す<br>GET バージョン")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="name",defaultValue="", value="名前を指定する",dataType = "string",paramType = "query",required = true),
+            @ApiImplicitParam(name="lang",defaultValue="", value="言語を指定する",dataType = "string",paramType = "query",required = false)
+    })
     @RequestMapping(method= RequestMethod.GET)
     public GreetingResponse greeting(@ModelAttribute @Validated GreetingRequest request) {
         return generateGreetingResponse(request);
     }
 
+    @ApiOperation(value="挨拶",notes = "名前をJSONで渡すと挨拶を返す")
     @RequestMapping(method = RequestMethod.POST)
     public GreetingResponse greetingOfJson(@RequestBody @Validated GreetingRequest request) {
         return generateGreetingResponse(request);
@@ -37,6 +44,7 @@ public class GreetingController {
         return new GreetingResponse(greeting);
     }
 
+    @ApiOperation(value="挨拶の履歴",notes = "挨拶の履歴を返す")
     @RequestMapping(value = "history", method = RequestMethod.GET)
     public HistoryResponse history() {
         GreetingHistory history = greetingService.history();
